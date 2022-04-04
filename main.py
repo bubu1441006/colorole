ed0c6283a961386abdcc86e741ab450f6b6846bb
bubu <hoangdung.bubu@gmail.com>
2022-04-04 16:47:13 +0700

    kinda functional now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import re
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -8,12 +9,36 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content.startswith('$#'):
-            col = message.content[2::]
+        if message.content.startswith('#'):
+            col = message.content.upper()
+
             print(col)
-            user = message.author
-            role = await message.guild.create_role(name=col, permissions=discord.Permissions(0), colour=discord.Colour(int('0x' + col, 16)))
-            await user.add_roles(message.guild.get_role(role.id))
+            if (re.match(r"^#[0-9A-F]{6}$", col)):
+                user = message.author
+
+                # delete previous roles
+                to_be_deleted = []
+                for r in user.roles:
+                    if (re.match(r"^[0-9A-F]{6}$", r.name.upper())):
+                        to_be_deleted.append(r)
+
+                for r in to_be_deleted:
+                    await user.remove_roles(r)
+
+                # create and assign new role
+                col = col[1::]
+                rid = -1
+                for r in message.guild.roles:
+                    if r.name == col:
+                        rid = r.id
+                
+                if rid == -1:
+                    role = await message.guild.create_role(name=col, permissions=discord.Permissions(0), colour=discord.Colour(int('0x' + col, 16)))
+                    rid = role.id
+
+                await user.add_roles(message.guild.get_role(rid))
+            else:
+                await message.channel.send('https://www.w3schools.com/colors/colors_hexadecimal.asp')
 
 client = MyClient()
-client.run('OTYwNDIwNjgyMzg1MjIzNjgw.YkqLdg.kmW0WthTW6eSbYFqjtuhEPSj8d0')
+client.run('OTYwNDIwNjgyMzg1MjIzNjgw.YkqLdg.dhqr_--XPjo9Bf1h9vfb0AszFJU')
